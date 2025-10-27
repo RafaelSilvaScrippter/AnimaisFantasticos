cc54bc3bd1bf0c7aa99b8911b5be4aba7d63ec4d
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-10-27 20:27:26 -0300

    Scroll suvae concluido

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 import tabNav from "./scripts/tabNav.js";
+import scrollSuave from "./scripts/scrollSUave.js";
 
 tabNav();
+scrollSuave();
--- scripts/tabNav.js
+++ scripts/tabNav.js
@@ -1,19 +1,21 @@
 const imgTab = document.querySelectorAll("[data-img-tab]");
 const conteudoTabNav = document.querySelectorAll("[data-tab-conteudo]");
 
-function navPorTab(item, index) {
-  if (index !== undefined) {
-    conteudoTabNav.forEach((item) => {
-      item.classList.remove("ativo");
-    });
-    conteudoTabNav[index].classList.add("ativo");
+function navegacaoPorTab() {
+  function navPorTab(item, index) {
+    if (index !== undefined) {
+      conteudoTabNav.forEach((item) => {
+        item.classList.remove("ativo");
+      });
+      conteudoTabNav[index].classList.add("ativo");
+    }
   }
+
+  imgTab.forEach((itemImg, index) => {
+    itemImg.addEventListener("click", () => {
+      navPorTab(itemImg, index);
+    });
+  });
 }
 
-imgTab.forEach((itemImg, index) => {
-  itemImg.addEventListener("click", () => {
-    navPorTab(itemImg, index);
-  });
-});
-
-export default navPorTab;
+export default navegacaoPorTab;
