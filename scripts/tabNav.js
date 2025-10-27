cf0cb9e0a2f9a5c191c8d6b3c9a1fda193804ef7
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-10-27 20:01:37 -0300

    Animacao das tabnav concluida e JavaScript das tabnav concluida

---
+++ scripts/tabNav.js
@@ -0,0 +1,19 @@
+const imgTab = document.querySelectorAll("[data-img-tab]");
+const conteudoTabNav = document.querySelectorAll("[data-tab-conteudo]");
+
+function navPorTab(item, index) {
+  if (index !== undefined) {
+    conteudoTabNav.forEach((item) => {
+      item.classList.remove("ativo");
+    });
+    conteudoTabNav[index].classList.add("ativo");
+  }
+}
+
+imgTab.forEach((itemImg, index) => {
+  itemImg.addEventListener("click", () => {
+    navPorTab(itemImg, index);
+  });
+});
+
+export default navPorTab;
