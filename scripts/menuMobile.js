c31dcebf7ca78709671303c9549e909c991f375d
RafaelSilvaScrippter <nativaeleven@gmail.com>
2025-10-30 14:59:37 -0300

    MenuMobile

---
+++ scripts/menuMobile.js
@@ -0,0 +1,19 @@
+import outsideClick from "./outsideClick.js";
+const btnMenuMobile = document.querySelector("[data-btn-menu-mobile]");
+const navMenuMobile = document.querySelector("[data-container-menu]");
+const containerPaiMenu = document.querySelector("[data-comparar]");
+
+function menuMobile() {
+  function abrirMenu() {
+    navMenuMobile.classList.toggle("ativo");
+    btnMenuMobile.classList.toggle("ativo");
+  }
+
+  btnMenuMobile.addEventListener("click", () => {
+    abrirMenu();
+  });
+
+  outsideClick(navMenuMobile, containerPaiMenu, "ativo");
+}
+
+export default menuMobile;
--- scripts/scrollSUave.js
+++ scripts/scrollSUave.js
@@ -2,18 +2,19 @@ const linksNavegacao = document.querySelectorAll("[data-link]");
 const conteudoLink = document.querySelectorAll("[data-conteudo]");
 
 function scrollSuave() {
-  function handleClick(index) {
-    const distanciaScroll = conteudoLink[index].getBoundingClientRect().top;
-    const somarDistancia = distanciaScroll - window.scrollY;
+  function handleClick(event, index) {
+    event.preventDefault();
+    const distanciaScroll = conteudoLink[index].offsetTop;
+    console.log(distanciaScroll);
     window.scrollTo({
-      top: somarDistancia,
+      top: distanciaScroll,
       behavior: "smooth",
     });
   }
 
   linksNavegacao.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      handleClick(index);
+    item.addEventListener("click", (event) => {
+      handleClick(event, index);
     });
   });
 }
